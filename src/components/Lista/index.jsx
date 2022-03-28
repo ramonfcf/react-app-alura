dc2b41b1c592edc4df755481689f727290d48f77
ramonfcf <ramonfcf@gmail.com>
2022-03-28 16:12:55 -0300

    entendendo utilização css/scss no react

--- src/components/Lista/index.jsx
+++ src/components/Lista/index.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import './style.scss'
 
 function Lista() {
-    return(
-        <aside>
 
+    const tarefas = [{
+        tarefa: 'React',
+        tempo: '02:00:00'
+    }, {
+        tarefa: 'JavaScript',
+        tempo: '01:00:00'
+    }, {
+        tarefa: 'Ruby',
+        tempo: '01:30:00'
+    }]
+    return(
+        <aside className='listaTarefas'>
+            <h2>Estudos do dia</h2>
+            <ul>
+                {tarefas.map((item, index) => (
+                    <li key={index} className='item'>
+                        <h3>{item.tarefa}</h3>
+                        <span>{item.tempo}</span>
+                    </li>
+                ))}
+            </ul>
         </aside>
     )
 }
