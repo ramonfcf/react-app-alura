825dacdf03116ee96d9639c2d7be9592c29549e9
ramonfcf <ramonfcf@gmail.com>
2022-03-29 14:05:39 -0300

    Adicionando componentes e css

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import style from './Botao.module.scss'
 
-class Botao extends React.Component{
-    render() {
-        return (
-            <button className={style.botao}>
-                Botão
-            </button>
-        )
-    }
+class Botao extends React.Component {
+   render() {
+      return (
+         <button className={style.botao}>
+            {this.props.children}
+         </button>
+      )
+   }
 }
 
 export default Botao;
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Cronometro from '../components/Cronometro';
 import Formulario from '../components/Formulario';
 import Lista from '../components/Lista';
 import style from './App.module.scss';
@@ -7,7 +8,8 @@ function App() {
   return (
     <div className={style.AppStyle}>
       <Formulario />
-      <Lista />  
+      <Lista />
+      <Cronometro />  
     </div>
   );
 }
